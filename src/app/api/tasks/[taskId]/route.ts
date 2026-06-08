887268c2151237a9b3b8de0aacb416e8120bca21
hclanka <152237163+chandudagoat@users.noreply.github.com>
2026-06-08 18:15:59 +0530

    added the create and read parts of the dashboard and now we can create a task and view it in our ui now i just have to add update and delete features

---
+++ src/app/api/tasks/[taskId]/route.ts
@@ -0,0 +1,79 @@
+import { NextResponse } from "next/server";
+import { db } from "@/db";
+import { tasks } from "@/db/schema";
+import { eq, and } from "drizzle-orm";
+import { requireAuth } from "@/lib/middleware";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { taskId: string } },
+) {
+  try {
+    const { user, errorResponse } = requireAuth(req);
+    if (errorResponse) return errorResponse;
+
+    const { taskId } = params;
+    const deletedTask = await db
+      .delete(tasks)
+      .where(and(eq(tasks.id, taskId), eq(tasks.userId, user!.userId)))
+      .returning();
+
+    if (deletedTask.length === 0) {
+      return NextResponse.json(
+        { error: "Task not found or unauthorized" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Task deleted successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Delete Task Error:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { taskId: string } },
+) {
+  try {
+    const { user, errorResponse } = requireAuth(req);
+    if (errorResponse) return errorResponse;
+    const { taskId } = params;
+    const body = await req.json();
+
+    const updatedTask = await db
+      .update(tasks)
+      .set({
+        ...(body.title && { title: body.title }),
+        ...(body.description && { description: body.description }),
+        ...(body.status && { status: body.status }),
+      })
+      .where(and(eq(tasks.id, taskId), eq(tasks.userId, user!.userId)))
+      .returning();
+
+    if (updatedTask.length === 0) {
+      return NextResponse.json(
+        { error: "Task not found or unauthorized" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Task updated successfully", task: updatedTask[0] },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Update Task Error:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
